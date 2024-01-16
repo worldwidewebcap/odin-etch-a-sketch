63b69677cc0af570d6786711dc3ddec28939947b
Shroom <150519355+worldwidewebcap@users.noreply.github.com>
2024-01-16 03:09:15 -0500

    Add feature to change grid size

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 // Function to draw a grid
 function drawGrid(size) {
-    // Get the grid container element
+
+    function brandNew() { drawGrid(DEFAULT) }
+
+    // Select the grid container element
     const GRID_CONTAINER = document.querySelector("#grid-container");
 
+    // Select the submit button
+    const SUBMIT = document.querySelector('#submit-btn')
+
+    // When clicked, reload the grid with new size
+    SUBMIT.addEventListener('click', () => {
+        const MAX = 100;
+        const MIN = 2
+        const text = document.querySelector('.text')
+        let inputField = document.querySelector('#grid-size')
+        let inputValue = inputField.value
+        if (inputValue > MAX || inputValue < MIN) {
+            alert(`Max Size: 100\nMin Size: 2`)
+            location.reload()
+        } else {
+            GRID_CONTAINER.innerHTML = ''
+            drawGrid(inputValue)
+        }
+    })
+
+    // Set the size of the grid
     GRID_CONTAINER.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     GRID_CONTAINER.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+
 
     // Loop through the cells
     for (let i = 0; i < size * size; i++) {
@@ -23,10 +47,16 @@ function drawGrid(size) {
 
     // Select the reset button
     const RESET = document.querySelector('#reset-btn')
+
+    // When clicked, reset the to default settings
     RESET.addEventListener('click', () => {
         GRID_CONTAINER.innerHTML = ''
-        drawGrid(50, 50)
+        drawGrid(DEFAULT)
     })
 }
 
-drawGrid(50, 50);
+// Set the default grid size
+const DEFAULT = 20;
+// Draw the grid
+drawGrid(DEFAULT);
+
