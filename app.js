6f1c524c81b503c55330af003dd007bacb213e1e
Shroom <150519355+worldwidewebcap@users.noreply.github.com>
2024-01-16 00:37:15 -0500

    Add feature to change cell color on hover

--- app.js
+++ app.js
@@ -2,23 +2,23 @@
 function drawGrid(size) {
     // Get the grid container element
     const GRID_CONTAINER = document.querySelector("#grid-container");
-  
+
     GRID_CONTAINER.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     GRID_CONTAINER.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-  
+
     // Loop through the cells
-    for (let i = 0; i < size*size; i++) {
-      // Create a cell element
-      const cell = document.createElement("div");
-      // Add a class to the cell element
-      cell.className = "cell";
-      // Set the cell element's id
-      cell.setAttribute("id", `${i}`);
-  
-      // Append the cell element to the grid container element
-      GRID_CONTAINER.appendChild(cell);
+    for (let i = 0; i < size * size; i++) {
+        // Create a cell element
+        const cell = document.createElement("div");
+        // Add a class to the cell element for styling
+        cell.className = "cell";
+        // On hover, change a cell's background color
+        cell.addEventListener('mouseover', (e) => {
+            cell.style.backgroundColor = 'white';
+        })
+        // Append the cell element to the grid container element
+        GRID_CONTAINER.appendChild(cell);
     }
-  }
-  
-  drawGrid(50, 50);
-  
+}
+
+drawGrid(50, 50);
