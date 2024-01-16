0e2d2c7f3b4ab24e9634957b0aa11d24a90cee6a
Shroom <150519355+worldwidewebcap@users.noreply.github.com>
2024-01-15 22:04:07 -0500

    Select and empty the grid container

---
+++ app.js
@@ -0,0 +1,9 @@
+// Function to draw a grid
+function drawGrid(rows, cols) {
+    
+    // Get the grid container element
+    const GRID_CONTAINER = document.querySelector('#grid-container')
+
+    // Clear the grid container
+    GRID_CONTAINER.innerHTML = ''
+}
